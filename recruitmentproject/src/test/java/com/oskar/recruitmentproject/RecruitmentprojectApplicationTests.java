3a8bde6be75a41972e8f576b047c2590aa396460
Ostin-Oskarose <oskarcraft@gmail.com>
2024-02-05 12:16:26 +0100

    Added starter spring boot project

---
+++ recruitmentproject/src/test/java/com/oskar/recruitmentproject/RecruitmentprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskar.recruitmentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
